9641bf12a84bfa33bcf04d3c327c6de8277faa1f
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2025-11-24 15:47:09 +0500

    admin

--- app/apps.py
+++ app/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+    
+
+class AccountsConfig(AppConfig):
+    name = 'accounts'
+
+    def ready(self):
+        import accounts.signals
+
